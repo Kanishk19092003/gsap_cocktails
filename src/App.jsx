89613732945f095251f2a25994fb09ff145b3c94
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-01-17 23:37:25 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import {ScrollTrigger, SplitText} from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
+
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
@@ -7,7 +9,7 @@ gsap.registerPlugin(ScrollTrigger,SplitText);
 const App = () => {
   return (
     <>
-     <h1>Hello, Gsap!</h1>
+     <Navbar/>
     </>
   )
 }
