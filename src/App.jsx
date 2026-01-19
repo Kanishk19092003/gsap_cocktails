b88b3da152d710b8228409eb5045893ec6710942
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-01-19 19:50:29 +0530

    Implement the about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/ABOUT.JSX";
 
 
 
@@ -15,6 +16,7 @@ const App = () => {
      <Navbar/>
      <Hero/>
      <Cocktails/>
+     <About/>
     </>
   )
 }
